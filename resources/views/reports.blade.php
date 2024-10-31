344246980a9a2ffd45eb703a5a3607a959565421
Aadil Safi <aadilxafi@gmail.com>
2024-10-31 10:21:57 +0500

    modal fixed

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -303,16 +303,18 @@
     </div>
 </div>
 <!-- Bootstrap Modal Structure -->
-<div class="modal fade" id="fileNotFoundModal" tabindex="-1" role="dialog" aria-labelledby="fileNotFoundModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content modal-dialog-centered">
+<div class="modal fade" id="fileNotFoundModal" tabindex="-1" role="dialog" aria-labelledby="fileNotFoundModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="fileNotFoundModalLabel">Schedule Not Available</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                 </button>
             </div>
             <div class="modal-body">
-                Something must have happened during upload. If the schedule has not been published, you can ask the store to resubmit.
+                Something must have happened during upload. If the schedule has not been published, you can ask the
+                store to resubmit.
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
