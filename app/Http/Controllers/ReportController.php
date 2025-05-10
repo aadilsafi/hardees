b481c8e42da63e0d13ba4dc59fe8b1dcecc03666
Aadil Safi <aadilxafi@gmail.com>
2025-05-10 22:00:45 +0500

    fixed the central time

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -121,7 +121,7 @@ class ReportController extends Controller
             // Only send email if there are metrics outside of standards
             if (!empty($metricsOutsideStandards)) {
                 // Create the message with the specific metrics that are outside standards
-                $message = "Schedule for unit {$report->UnitNo} for week of {$report->ScheduleDate} was approved by {$report->ApprovedBy} Central\n\n";
+                $message = "Schedule for unit {$report->UnitNo} for week of {$report->ScheduleDate} was approved by {$report->ApprovedBy} Central Time.\n\n";
                 $message .= "This schedule's has metrics that fall outside of company standards as listed below:\n\n";
                 $message .= implode("\n\n", $metricsOutsideStandards);
 
